2cd09ea9b1c21612db49b4b44854669929a8aed9
Jesse Overright <jesseoverright@gmail.com>
2014-10-20 14:35:14 -0500

    Finalized radio boxes

--- includes/postmeta-types/radio.php
+++ includes/postmeta-types/radio.php
@@ -1,23 +1,41 @@
 <?php
 
 class WP_RadioMeta extends WP_SelectMeta {
+    /**
+     * Radio input type
+     * @var string
+     */
     protected $input_type = 'radio';
 
-    public function display_postmeta( $post_id ) {
+    /**
+     * Displays the radio buttons in the WP admin area
+     * @param  $data content
+     * @return html       radio buttons
+     */
+    public function display_input( $data ) {
         foreach ( $this->choices as $key => $value ) {
-            echo "<input type=\"{$this->input_type}\" name=\"{$this->key} ";
+            echo "<input type=\"{$this->input_type}\" name=\"{$this->key}\"";
             if ( $this->has_custom_labels ) {
-                echo "value=\"{$key}\"";
+                echo " value=\"{$key}\"";
                 if ( $key == $data ) {
                     echo " checked";
                 }
             } else {
+                echo " value=\"{$value}\"";
                 if ( $value == $data ) {
                     echo " checked";
                 }
             }
             
-            echo " value=\"{$value}\"><br>";
+            echo ">{$value}<br>";
         }
     }
+}
+
+class WP_BooleanMeta extends WP_RadioMeta {
+    /**
+     * The default choices boolean meta type
+     * @var array
+     */
+    protected $choices = array ( 'true' => 'True', 'false' => 'False' );
 }
