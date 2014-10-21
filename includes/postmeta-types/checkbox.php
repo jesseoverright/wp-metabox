8ff9b00baf929b01b9013b3009796a49644eda91
Jesse Overright <jesseoverright@gmail.com>
2014-10-20 19:16:40 -0500

    Added comments and fixed bugs in checkbox

--- includes/postmeta-types/checkbox.php
+++ includes/postmeta-types/checkbox.php
@@ -3,21 +3,27 @@
 class WP_CheckboxMeta extends WP_SelectMeta {
     protected $input_type = 'checkbox';
 
-    public function display_postmeta( $post_id ) {
+    /**
+     * Displays the checkbox items in the WP admin
+     * @param  int $post_id  individual post id
+     * @return html          input content
+     */
+    public function display_input( $data ) {
         foreach ( $this->choices as $key => $value ) {
-            echo "<input type=\"{$this->input_type}\" name=\"{$this->key} ";
+            echo "<input type=\"{$this->input_type}\" name=\"{$this->key}\"";
             if ( $this->has_custom_labels ) {
-                echo "value=\"{$key}\"";
+                echo " value=\"{$key}\"";
                 if ( $key == $data ) {
                     echo " checked";
                 }
             } else {
+                echo " value=\"{$value}\"";
                 if ( $value == $data ) {
                     echo " checked";
                 }
             }
             
-            echo " value=\"{$value}\"><br>";
+            echo ">{$value}<br>";
         }
     }
 }
