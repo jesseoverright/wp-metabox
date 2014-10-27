85052cdc871ae1014e51b852a7f08a9ac6f35bbc
Jesse Overright <jesseoverright@gmail.com>
2014-10-26 21:33:56 -0500

    Fixed issues with checkbox postmeta saving properly

--- includes/postmeta-types/checkbox.php
+++ includes/postmeta-types/checkbox.php
@@ -9,21 +9,54 @@ class WP_CheckboxMeta extends WP_SelectMeta {
      * @return html          input content
      */
     public function display_input( $data ) {
+        if ( ! is_array ( $data ) ) {
+            $data = array();
+        }
         foreach ( $this->choices as $key => $value ) {
-            echo "<input type=\"{$this->input_type}\" name=\"{$this->key}\"";
+            echo "<input type=\"{$this->input_type}\" name=\"{$this->key}[]\"";
             if ( $this->has_custom_labels ) {
                 echo " value=\"{$key}\"";
-                if ( $key == $data ) {
+                if ( in_array( $key, $data ) ) {
                     echo " checked";
                 }
             } else {
                 echo " value=\"{$value}\"";
-                if ( $value == $data ) {
+                if ( in_array( $value, $data ) ) {
                     echo " checked";
                 }
             }
             
             echo ">{$value}<br>";
+        }
+    }
+
+    /**
+     * Updates postmeta for checkbox array
+     * @param  id $post_id
+     * @param  array $data    checked boxes
+     */
+    public function update( $post_id, $data ) {
+        foreach ( $data as $key => $value ) {
+            if ( $this->has_custom_labels ) {
+                if ( ! array_key_exists( $value, $this->choices ) ) {
+                    unset( $data[ $key ] );
+                }
+            } else {
+                if ( ! in_array( $value, $this->choices ) ) {
+                    unset( $data[ $key ] );
+                }
+            }
+        }
+
+        # default postmeta save
+        if ( get_post_meta($post_id, $this->key) == '') {
+            add_post_meta($post_id, $this->key, $data, true);
+        }
+        elseif ( $data != get_post_meta($post_id, $this->key, true) ) {
+            update_post_meta($post_id, $this->key, $data);
+        }
+        elseif ( $data == '' ) {
+            delete_post_meta($post_id, $this->key, get_post_meta($post_id, $this->key, true));
         }
     }
 }
